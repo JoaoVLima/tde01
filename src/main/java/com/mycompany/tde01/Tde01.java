adcd3123af6e0df338372210c52e201f97edccac
JoaoVLima <celu.joaolima@gmail.com>
2025-09-04 09:44:17 -0300

    First commit

---
+++ src/main/java/com/mycompany/tde01/Tde01.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tde01;
+
+/**
+ *
+ * @author Lima
+ */
+public class Tde01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
